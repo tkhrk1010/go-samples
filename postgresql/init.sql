a9e5cfd15dc7f46fa22244c7b2d8f393845e4927
tkhrk <>
2024-03-26 18:00:45 +0900

    mod: postgresql init

--- postgresql/init.sql
+++ postgresql/init.sql
@@ -1,10 +1,9 @@
 DROP TABLE IF EXISTS album;
 CREATE TABLE album (
-  id         INT AUTO_INCREMENT NOT NULL,
+  id         SERIAL PRIMARY KEY,
   title      VARCHAR(128) NOT NULL,
   artist     VARCHAR(255) NOT NULL,
-  price      DECIMAL(5,2) NOT NULL,
-  PRIMARY KEY (`id`)
+  price      DECIMAL(5,2) NOT NULL
 );
 
 INSERT INTO album
