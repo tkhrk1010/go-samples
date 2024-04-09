0d79b4d77d8862e767fabd0a4f5462bbb23aaa75
tkhrk <>
2024-04-09 17:48:35 +0900

    mod: refactor actor model es

--- actor-model/eventstream/src/main.go
+++ actor-model/eventstream/src/main.go
@@ -7,6 +7,16 @@ import (
 	"github.com/asynkron/protoactor-go/eventstream"
 )
 
+// event streamをsubscribeするActor
+type SubscriberActor struct{}
+
+func (state *SubscriberActor) Receive(context actor.Context) {
+	switch msg := context.Message().(type) {
+	case ESEvent:
+		fmt.Printf("Subscriber received message: %v\n", msg.Message.Text)
+	}
+}
+
 // event streamに発行されるMessageの型
 type Message struct {
 	Text string
@@ -30,45 +40,39 @@ func NewPublisherActor(stream *eventstream.EventStream) *PublisherActor {
 
 func (state *PublisherActor) Receive(context actor.Context) {
 	switch msg := context.Message().(type) {
-  // PublisherActorがMessageを受け取ると、event streamにpublishする
+	// PublisherActorがMessageを受け取ると、event streamにpublishする
 	case Message:
 		event := ESEvent{Message: msg}
 		state.stream.Publish(event)
 	}
 }
 
-// 使ってない。SubscriberActorがstreamをSubscribeするためにはどうする？
-// あくまでevent streamにもPIDがあるので、ESがactorともいえる。はず。
-// type SubscriberActor struct{}
-
-func SubscriberReceive(context actor.Context) {
-	switch msg := context.Message().(type) {
-	case ESEvent:
-		fmt.Printf("Subscriber received message: %v\n", msg.Message.Text)
-	}
-}
 
 func main() {
 	fmt.Println("start")
 	system := actor.NewActorSystem()
-  stream := eventstream.NewEventStream()
+	stream := eventstream.NewEventStream()
 
-	fmt.Println("create publisherActor")
-	props := actor.PropsFromProducer(func() actor.Actor { return NewPublisherActor(stream) })
-	publisherActorPid, _ := system.Root.SpawnNamed(props, "publisherActor")
+	fmt.Println("create subscriberActor")
+  subscriberProps := actor.PropsFromProducer(func() actor.Actor { return &SubscriberActor{} })
+  subscriberPID := system.Root.Spawn(subscriberProps)
 
-	fmt.Println("subscibing...")
-	stream.Subscribe(func(event interface{}) {
-		system.Root.Send(system.Root.Spawn(actor.PropsFromFunc(SubscriberReceive)), event)
-	})
+  fmt.Println("subscribe to event stream")
+  stream.Subscribe(func(event interface{}) {
+      system.Root.Send(subscriberPID, event)
+  })
+
+  fmt.Println("create publisherActor")
+	publisherProps := actor.PropsFromProducer(func() actor.Actor { return NewPublisherActor(stream) })
+	publisherPid, _ := system.Root.SpawnNamed(publisherProps, "publisherActor")
 
 	fmt.Println("send message to publisherActor")
 	message := Message{Text: "Hello, Protoactor!"}
-	system.Root.Send(publisherActorPid, message)
+	system.Root.Send(publisherPid, message)
 
 	fmt.Println("wait for message to be published")
-  // goroutineを終了しないようにする
+	// goroutineを終了しないようにする
 	console := make(chan struct{})
-  // channelからの受信を待つ
+	// channelからの受信を待つ
 	<-console
 }
