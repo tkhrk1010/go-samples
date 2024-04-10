bbe0c61353d9132ba56270efe4f7f4075dd595cc
tkhrk <>
2024-04-10 18:30:56 +0900

    mod: actor-model persis minimal

--- actor-model/persistence/minimal/src/main.go
+++ actor-model/persistence/minimal/src/main.go
@@ -68,7 +68,6 @@ func (a *MyActor) Receive(ctx actor.Context) {
 	}
 }
 
-
 func main() {
 	log.Println("start")
 	system := actor.NewActorSystem()
@@ -100,8 +99,7 @@ func main() {
 	myActorPid := system.Root.Spawn(props)
 	log.Printf("MyActor PID: %s", myActorPid)
 
-
-	// 
+	//
 	// eventを永続化
 	log.Println("persist event")
 	// arg: actorName string, eventIndex int, event proto.Message
@@ -116,15 +114,16 @@ func main() {
 	// actorが永続化してくれるのを少し待つ
 	time.Sleep(2 * time.Second)
 
-
 	//
 	// eventを取得
 	log.Println("get event")
 	// actorNameは、context.Self().Idを使っているらしいことが公式からわかる。
 	// https://github.com/asynkron/protoactor-go/blob/2a5372b5b465b3bb030dd26086cb5840465e7354/persistence/plugin.go#L55
 	// そのため、actorNameは、actorのPIDを使う
-	provider.GetEvents(myActorPid.Id, 0, 2)
-
+	// 引数2: eventIndexStartは、取得したい最初のindexを指定する。
+	// 引数3: eventIndexEndは、取得したい最後のindexを指定する。全体のindexを超えているとerrorになる。0を指定すると全て取得らしい
+	// https://github.com/asynkron/protoactor-go/blob/2a5372b5b465b3bb030dd26086cb5840465e7354/persistence/in_memory_provider.go#L69C84-L69C97
+	provider.GetEvents(myActorPid.Id, 0, 0)
 
 	// systemが終了しないように待機
 	console := make(chan string)
