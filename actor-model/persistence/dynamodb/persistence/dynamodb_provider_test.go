d28b9067b16bb7966972ee6face9cfd9bddd0fd1
tkhrk <>
2024-05-02 00:05:56 +0900

    mod test

--- actor-model/persistence/dynamodb/persistence/dynamodb_provider_test.go
+++ actor-model/persistence/dynamodb/persistence/dynamodb_provider_test.go
@@ -2,10 +2,13 @@ package persistence_test
 
 import (
 	"testing"
+	"fmt"
 	"context"
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb/types"
 	"github.com/aws/aws-sdk-go-v2/feature/dynamodb/attributevalue"
+
 	"github.com/stretchr/testify/assert"
 	"google.golang.org/protobuf/proto"
 
@@ -13,31 +16,32 @@ import (
 )
 
 func TestNewProviderState(t *testing.T) {
-	clinet := InitializeDynamoDBClient()
-	ps := p.NewProviderState(clinet)
+	client := InitializeDynamoDBClient()
+	ps := p.NewProviderState(client)
 	if ps == nil {
 		t.Error("NewProviderState returned nil")
 	}
 }
 
 func TestGetState(t *testing.T) {
-	clinet := InitializeDynamoDBClient()
-	ps := p.NewProviderState(clinet)
+	client := InitializeDynamoDBClient()
+	ps := p.NewProviderState(client)
 	state := ps.GetState()
 	if state != ps {
 		t.Error("GetState should return the same instance")
 	}
 }
 
+// 呼び出せることだけ確認
 func TestRestart(t *testing.T) {
-	clinet := InitializeDynamoDBClient()
-	ps := p.NewProviderState(clinet)
+	client := InitializeDynamoDBClient()
+	ps := p.NewProviderState(client)
 	ps.Restart()
 }
 
 func TestGetSnapshotInterval(t *testing.T) {
-	clinet := InitializeDynamoDBClient()
-	ps := p.NewProviderState(clinet)
+	client := InitializeDynamoDBClient()
+	ps := p.NewProviderState(client)
 	interval := ps.GetSnapshotInterval()
 	if interval != 3 {
 		t.Errorf("GetSnapshotInterval should return 3, got: %d", interval)
@@ -45,6 +49,100 @@ func TestGetSnapshotInterval(t *testing.T) {
 }
 
 func TestGetSnapshot(t *testing.T) {
+	tableName := "snapshot"
+	client := InitializeDynamoDBClient()
+	ps := p.NewProviderState(client)
+	actorName := "testActor"
+	eventIndex := 1
+	snapshotData := &p.Snapshot{Data: "testSnapshot"}
+
+	// スナップショットを保存
+	snapshotBytes, err := proto.Marshal(snapshotData)
+	assert.NoError(t, err)
+	item := map[string]types.AttributeValue{
+		"actorName":  &types.AttributeValueMemberS{Value: actorName},
+		"eventIndex": &types.AttributeValueMemberN{Value: fmt.Sprintf("%d", eventIndex)},
+		"payload":   &types.AttributeValueMemberB{Value: snapshotBytes},
+	}
+	putInput := &dynamodb.PutItemInput{
+		TableName: aws.String(tableName),
+		Item:      item,
+	}
+	_, err = client.PutItem(context.Background(), putInput)
+	assert.NoError(t, err)
+
+	// GetSnapshotを呼び出して、保存したスナップショットを取得
+	retrievedSnapshot, retrievedEventIndex, ok := ps.GetSnapshot(actorName)
+	assert.True(t, ok)
+	assert.Equal(t, eventIndex, retrievedEventIndex)
+
+	snapshot, ok := retrievedSnapshot.(*p.Snapshot)
+	assert.True(t, ok)
+	assert.Equal(t, snapshotData.Data, snapshot.Data)
+
+	// 存在しないスナップショットを取得
+	_, _, ok = ps.GetSnapshot("nonexistentActor")
+	assert.False(t, ok)
+
+	// クリーンアップ
+	key, err := attributevalue.MarshalMap(map[string]interface{}{
+		"actorName":  actorName,
+		"eventIndex": eventIndex,
+	})
+	assert.NoError(t, err)
+
+	deleteInput := &dynamodb.DeleteItemInput{
+		Key:       key,
+		TableName: aws.String(tableName),
+	}
+	_, err = client.DeleteItem(context.Background(), deleteInput)
+	assert.NoError(t, err)
+}
+
+func TestPersistSnapshot(t *testing.T) {
+	tableName := "snapshot"
+	client := InitializeDynamoDBClient()
+	ps := p.NewProviderState(client)
+
+	actorName := "testActor"
+	eventIndex := 1
+	snapshotData := &p.Snapshot{Data: "testSnapshot"}
+
+	ps.PersistSnapshot(actorName, eventIndex, snapshotData)
+
+	// 保存されたスナップショットを取得して検証
+	retrievedSnapshot, retrievedEventIndex, ok := ps.GetSnapshot(actorName)
+	assert.True(t, ok)
+	assert.Equal(t, eventIndex, retrievedEventIndex)
+
+	snapshot, ok := retrievedSnapshot.(*p.Snapshot)
+	assert.True(t, ok)
+
+	assert.Equal(t, snapshotData.Data, snapshot.Data)
+
+	// クリーンアップ
+	key, err := attributevalue.MarshalMap(map[string]interface{}{
+		"actorName":  actorName,
+		"eventIndex": eventIndex,
+	})
+	assert.NoError(t, err)
+
+	deleteInput := &dynamodb.DeleteItemInput{
+		Key:       key,
+		TableName: aws.String(tableName),
+	}
+	_, err = client.DeleteItem(context.Background(), deleteInput)
+	assert.NoError(t, err)
+}
+
+// 呼び出せることだけ確認
+func TestDeleteSnapshots(t *testing.T) {
+	client := InitializeDynamoDBClient()
+	ps := p.NewProviderState(client)
+	ps.DeleteSnapshots("testActor", 1)
+}
+
+func TestGetEvents(t *testing.T) {
 	tableName := "journal"
 
 	client := InitializeDynamoDBClient()
@@ -115,34 +213,56 @@ func TestGetSnapshot(t *testing.T) {
 	}
 }
 
-func TestPersistSnapshot(t *testing.T) {
-	clinet := InitializeDynamoDBClient()
-	ps := p.NewProviderState(clinet)
-	ps.PersistSnapshot("testActor", 1, &p.Snapshot{})
-}
-
-// 呼び出せることだけ確認
-func TestDeleteSnapshots(t *testing.T) {
-	clinet := InitializeDynamoDBClient()
-	ps := p.NewProviderState(clinet)
-	ps.DeleteSnapshots("testActor", 1)
-}
-
-func TestGetEvents(t *testing.T) {
-	clinet := InitializeDynamoDBClient()
-	ps := p.NewProviderState(clinet)
-	ps.GetEvents("testActor", 1, 2, func(e interface{}) {})
-}
-
 func TestPersistEvent(t *testing.T) {
-	clinet := InitializeDynamoDBClient()
-	ps := p.NewProviderState(clinet)
-	ps.PersistEvent("testActor", 1, &p.Event{})
+	tableName := "journal"
+
+	client := InitializeDynamoDBClient()
+	ps := p.NewProviderState(client)
+
+	actorName := "testActor"
+	eventIndex := 1
+	eventData := &p.Event{Data: "testEvent"}
+
+	ps.PersistEvent(actorName, eventIndex, eventData)
+
+	// 保存したイベントの検証
+	key, err := attributevalue.MarshalMap(map[string]interface{}{
+		"actorName":  actorName,
+		"eventIndex": eventIndex,
+	})
+	assert.NoError(t, err)
+	input := &dynamodb.GetItemInput{
+		Key:       key,
+		TableName: aws.String(tableName),
+	}
+	result, err := client.GetItem(context.Background(), input)
+	assert.NoError(t, err)
+
+	var persistedEvent map[string]interface{}
+	err = attributevalue.UnmarshalMap(result.Item, &persistedEvent)
+	assert.NoError(t, err)
+
+	assert.Equal(t, actorName, persistedEvent["actorName"])
+	// Goでは、UnmarshalMapすると数値はfloat64になるため、intに変換する
+	assert.Equal(t, eventIndex, int(persistedEvent["eventIndex"].(float64)))
+
+	var persistedEventData p.Event
+	err = proto.Unmarshal(persistedEvent["payload"].([]byte), &persistedEventData)
+	assert.NoError(t, err)
+	assert.Equal(t, eventData.Data, persistedEventData.Data)
+
+	// クリーンアップ
+	deleteInput := &dynamodb.DeleteItemInput{
+		Key:       key,
+		TableName: aws.String(tableName),
+	}
+	_, err = client.DeleteItem(context.Background(), deleteInput)
+	assert.NoError(t, err)
 }
 
 // 呼び出せることだけ確認
 func TestDeleteEvents(t *testing.T) {
-	clinet := InitializeDynamoDBClient()
-	ps := p.NewProviderState(clinet)
+	client := InitializeDynamoDBClient()
+	ps := p.NewProviderState(client)
 	ps.DeleteEvents("testActor", 1)
 }
